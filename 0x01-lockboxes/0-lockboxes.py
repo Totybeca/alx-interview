01f15dcbc6e1b42cdb558474ba0e2897f9e6eab1
Totybeca <emeraldtoyin@gmail.com>
2023-04-27 22:32:08 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,34 @@
 #!/usr/bin/python3
-from collections import deque
 
-def canUnlockAll(boxes):
-    n = len(boxes)
-    visited = [False] * n  # initialize all boxes as unvisited
-    visited[0] = True  # mark the first box as visited
-    queue = deque([0])  # start BFS from the first box
+def can_unlock_all(boxes):
+    """
+    Determines if all the boxes can be opened.
 
-    while queue:
-        box = queue.popleft()
-        for key in boxes[box]:
-            if key < n and not visited[key]:
-                visited[key] = True
-                queue.append(key)
+    Args:
+        boxes (list): A list of lists representing the boxes.
 
-    return all(visited)
+    Returns:
+        True if all the boxes can be opened, False otherwise.
+    """
+    num_boxes = len(boxes)  # Number of boxes in the list
+    box_dict = {}  # Dictionary containing the boxes that can be opened
+    index = 0  # Index of the current box being checked
+
+    for box in boxes:  # Iterate over each box in the list
+        if not isinstance(box, list):
+            raise ValueError("Input must be a list of lists.")
+        
+        if len(box) == 0 or index == 0:
+            box_dict[index] = -1
+
+        for key in box:
+            if key < num_boxes and key != index:
+                box_dict[key] = key
+
+        if len(box_dict) == num_boxes:
+            return True  # All boxes can be opened
+
+        index += 1
+
+    return False  # Not all boxes can be opened
 
