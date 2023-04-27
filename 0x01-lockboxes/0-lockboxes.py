07f2a1c3fdf63439a40f00c7717820a514541dc0
Totybeca <emeraldtoyin@gmail.com>
2023-04-27 23:14:20 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,15 +1,16 @@
-sizes = len(boxes)  # The sizes of the list of boxes.
-    box = {}  # The dictionary that will contain the boxes to  be opened.
-    a = 0  #  the box that will be checked.
+#!/usr/bin/python3
+"""This script will unlock list of lists"""
 
 
-for keys in boxes:  # for each of the boxes in the list of boxes.
-    if len(keys) == 0 or a == 0:
-        box[a] = -1
+def canUnlockAll(boxes):
+    """This function takes the list of lists and the content
+       of the list will unlock other lists.
+    """
+    keys = [0]
     for key in keys:
-        if key < sizes and key != a:
-            box[key] = key  # add box to dictionary.
-    if len(box) == sizes:
-        return True  # all the boxes can now  be opened.
-    a += 1
-return False
+        for boxKeys in boxes[key]:
+            if boxKeys not in keys and boxKeys < len(boxes):
+                keys.append(boxKeys)
+    if len(keys) == len(boxes):
+        return True
+    return False
