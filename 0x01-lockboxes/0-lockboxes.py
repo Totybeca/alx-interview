ccb1f6014d706c85c7ab070a2226aeb97aac2d9a
Totybeca <emeraldtoyin@gmail.com>
2023-04-27 22:44:44 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,34 +1,18 @@
 #!/usr/bin/python3
+"""This script will unlock the list of lists."""
+"""Module that determines if all the boxes can be opened."""
 
-def can_unlock_all(boxes):
+
+def canUnlockAll(boxes):
+    """function takes a list of lists and the content
+       of a list unlocks other lists.
     """
-    Determines if all the boxes can be opened.
 
-    Args:
-        boxes (list): A list of lists representing the boxes.
-
-    Returns:
-        True if all the boxes can be opened, False otherwise.
-    """
-    num_boxes = len(boxes)  # Number of boxes in the list
-    box_dict = {}  # Dictionary containing the boxes that can be opened
-    index = 0  # Index of the current box being checked
-
-    for box in boxes:  # Iterate over each box in the list
-        if not isinstance(box, list):
-            raise ValueError("Input must be a list of lists.")
-        
-        if len(box) == 0 or index == 0:
-            box_dict[index] = -1
-
-        for key in box:
-            if key < num_boxes and key != index:
-                box_dict[key] = key
-
-        if len(box_dict) == num_boxes:
-            return True  # All boxes can be opened
-
-        index += 1
-
-    return False  # Not all boxes can be opened
-
+    keys = [0]
+    for key in keys: 
+        for keyBox in boxes[key]:   # for each box in the list of boxes.
+            if keyBox not in keys and keyBox < len(boxes):
+                keys.append(keyBox)
+    if len(keys) == len(boxes):
+        return True # all the boxes can be opened.
+    return False
