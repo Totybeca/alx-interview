7c0dad4f494cf631ab8cd8c92a7de4512cd5cc6d
Totybeca <emerald@gmail.com>
2023-06-14 13:16:37 +0000

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,19 @@
 #!/usr/bin/python3
-"""A function to determine the fewest number of coins needed
-   to meet a given amount total"""
+'''Making Change module'''
 
 
 def makeChange(coins, total):
-    """This function will take a list of coins and use
-       that to calculate how much change the total will require
-    """
-    if total <= 0:
+    '''Function that makes change for a given total'''
+    if total < 1:
         return 0
-
-    else:
-        coin = sorted(coins)
-        coin.reverse()
-        counter = 0
-        for i in coin:
-            while(total >= i):
-                counter += 1
-                total -= i
+    change = 0
+    coins.sort(reverse=True)
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
         if total == 0:
-            return counter
+            return change
+    if total != 0:
         return -1
 
